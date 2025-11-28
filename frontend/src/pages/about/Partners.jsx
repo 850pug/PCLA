4c7460fedcbae44107bbb80e600cedfc1ebcb512
850pug <scott@thecreativecoefficient.com>
2025-11-29 09:15:39 +1000

    Update Partners.jsx

--- frontend/src/pages/about/Partners.jsx
+++ frontend/src/pages/about/Partners.jsx
@@ -96,7 +96,7 @@ export const Partners = () => {
           <div className="max-w-4xl mx-auto text-center">
             <h2 className="text-4xl font-bold text-gray-900 mb-6">Building a Network of Support</h2>
             <p className="text-xl text-gray-700 leading-relaxed">
-              We welcome interest from community groups and organizations who would like to discuss potential partnerships.Since our opening in 1982, we have worked in partnership with other government and non-governmental organizations to provide a range of unique services for people with a serious or persistent mental illness. If you are interested in partnering with PCLA, please contact Michael Kierszenblat, Executive Director mkierszenblat@pclaservices.ca.
+              We welcome interest from community groups and organizations who would like to discuss potential partnerships.Since our opening in 1982, we have worked in partnership with other government and non-governmental organizations to provide a range of unique services for people with a serious or persistent mental illness.
             </p>
             <p className="text-xl text-gray-700 leading-relaxed">
               If you are interested in partnering with PCLA, please contact Michael Kierszenblat, Executive Director mkierszenblat@pclaservices.ca.
