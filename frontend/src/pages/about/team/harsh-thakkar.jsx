44d0c732cddbfd8d9dbfc8521fdb9ef4c5d6baf4
850pug <scott@thecreativecoefficient.com>
2025-12-02 09:13:56 +1000

    Update harsh-thakkar.jsx

--- frontend/src/pages/about/team/harsh-thakkar.jsx
+++ frontend/src/pages/about/team/harsh-thakkar.jsx
@@ -20,7 +20,7 @@ export const HarshThakkar = () => {
             <div className="md:col-span-1">
               <div className="aspect-[3/4] w-full overflow-hidden rounded-3xl border-4 border-gray-200 shadow-xl">
                 <img 
-                  src="/images/harsh-thakkar-headshot.jpg" 
+                  src="/images/Headshot-placeholder.jpeg" 
                   alt="Harsh Thakkar"
                   className="w-full h-full object-cover"
                 />
