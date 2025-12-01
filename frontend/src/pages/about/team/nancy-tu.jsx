3cfd8b885736bce4571aa633cbc60ec7dc003f74
850pug <scott@thecreativecoefficient.com>
2025-12-02 09:15:07 +1000

    Update nancy-tu.jsx

--- frontend/src/pages/about/team/nancy-tu.jsx
+++ frontend/src/pages/about/team/nancy-tu.jsx
@@ -20,7 +20,7 @@ export const NancyTu = () => {
             <div className="md:col-span-1">
               <div className="aspect-[3/4] w-full overflow-hidden rounded-3xl border-4 border-gray-200 shadow-xl">
                 <img 
-                  src="/images/nancy-tu-headshot.jpg" 
+                  src="/images/Headshot-placeholder.jpeg" 
                   alt="Nancy Tu"
                   className="w-full h-full object-cover"
                 />
