67516338ee11ab27c98782c53928d004cae2627a
850pug <scott@thecreativecoefficient.com>
2025-12-02 09:10:44 +1000

    Update anastasia-humenuk.jsx

--- frontend/src/pages/about/team/anastasia-humenuk.jsx
+++ frontend/src/pages/about/team/anastasia-humenuk.jsx
@@ -23,7 +23,7 @@ export const AnastasiaHumenuk = () => {
             <div className="md:col-span-1">
               <div className="aspect-[3/4] w-full overflow-hidden rounded-3xl border-4 border-gray-200 shadow-xl">
                 <img 
-                  src="/images/anastasia-humenuk-headshot.jpg" 
+                  src="/images/Headshot-placeholder.jpeg" 
                   alt="Anastasia Humenuk"
                   className="w-full h-full object-cover"
                 />
